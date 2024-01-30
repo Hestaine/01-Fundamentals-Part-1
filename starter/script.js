260d423021ad0dda3b2de50e690be3543edb3483
Hestaine <ariffhestaine@gmail.com>
2024-01-30 12:00:42 +0100

    yohohoho

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,61 @@
+/*
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is fun");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+
+// Variable name convention
+let Jonas_Matilda = "Jm";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 100);
+// console.log(typeof "javascriptIsFun");
+
+javascriptIsFun = "yes!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2017;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schnedmann";
+console.log(lastName);
+lastName = "Hackerman";
+console.log(lastName);
+*/
