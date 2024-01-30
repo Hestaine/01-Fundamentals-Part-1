64b25dda1f32435c9b8de2a035ecad7645b773a6
Hestaine <ariffhestaine@gmail.com>
2024-01-30 12:39:21 +0100

    yoooo

--- starter/script.js
+++ starter/script.js
@@ -58,4 +58,34 @@ lastName = "Schnedmann";
 console.log(lastName);
 lastName = "Hackerman";
 console.log(lastName);
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah, "yay");
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//  2 ** 3 means 2 to the power of 3 which is 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "Schmedtman";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10;
+x *= 4; // x = x * 4;
+x /= 2; // x = x / 2;
+x++; // x = x + 1;
+x--; // x = x - 1;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
 */
